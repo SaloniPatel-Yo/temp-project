88177b373018b57d743d04f169e46ffdc8544dd9
MayankShah-Yo <73048084+MayankShah-Yo@users.noreply.github.com>
2020-10-18 09:33:56 +0530

    Removing unrequited +1

--- selection.py
+++ selection.py
@@ -9,7 +9,7 @@ for i in range(len(A)):
 	# Find the minimum element in remaining 
 	# unsorted array 
 	min_idx = i 
-	for j in range(i+1, len(A)): 
+	for j in range(i, len(A)): 
 		if A[min_idx] > A[j]: 
 			min_idx = j 
 			
